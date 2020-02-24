bf91a2b189d326070da77d87a21fedf490defd41
乐正白芷 <42693943+yuezhengbaizhi@users.noreply.github.com>
2020-02-24 00:25:50 -0500

    Update make_cache.sh

--- make_cache.sh
+++ make_cache.sh
@@ -2,9 +2,11 @@
 
 . build/envsetup.sh
 lunch hentai_poplar-user
+
 echo " CCCCCC     A     CCCCCC H    H EEEEEE"
 echo " C         A A    C      H    H E     "
 echo " C        A   A   C      HHHHHH EEEEE "
 echo " C       AAAAAAA  C      H    H E     "
 echo " CCCCCC A       A CCCCCC H    H EEEEEE"
+
 make cacheimage
