75c25c2a6831d4004aa696be6fb379f523003322
乐正白芷 <42693943+yuezhengbaizhi@users.noreply.github.com>
2020-02-24 01:21:10 -0500

    Create make_system_go.sh

---
+++ make_system_go.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cd ~/hentai
+
+. build/envsetup.sh
+lunch hentai_poplar_go-user
+
+echo " SSSSSS Y   Y SSSSSS TTTTTTT EEEEEE M     M"
+echo " S       Y Y  S         T    E      MM   MM"
+echo " SSSSSS   Y   SSSSSS    T    EEEEE  M M M M"
+echo "      S   Y        S    T    E      M  M  M"
+echo " SSSSSS   Y   SSSSSS    T    EEEEEE M     M"
+
+make systemimage
