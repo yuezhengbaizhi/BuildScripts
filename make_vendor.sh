b861c7608319fbe17969d6f116b070d9f312e07f
乐正白芷 <42693943+yuezhengbaizhi@users.noreply.github.com>
2020-02-24 00:28:18 -0500

    Create make_vendor.sh

---
+++ make_vendor.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+. build/envsetup.sh
+lunch hentai_poplar-user
+
+echo " V       V EEEEEE N   N DDDD   OOOOOO RRRRRR"
+echo "  V     V  E      NN  N D   D  O    O R    R"
+echo "   V   V   EEEEE  N N N D    D O    O RRRRRR"
+echo "    V V    E      N  NN D   D  O    O R RR  "
+echo "     V     EEEEEE N   N DDDD   OOOOOO R   RR"
+
+make vendorimage
