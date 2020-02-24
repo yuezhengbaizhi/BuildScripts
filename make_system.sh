bfe47796a28aa7f2e69e177ea2a90aa523f06ed6
乐正白芷 <42693943+yuezhengbaizhi@users.noreply.github.com>
2020-02-24 00:28:57 -0500

    Create make_system.sh

---
+++ make_system.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+. build/envsetup.sh
+lunch hentai_poplar-user
+
+echo " SSSSSS Y   Y SSSSSS TTTTTTT EEEEEE M     M"
+echo " S       Y Y  S         T    E      MM   MM"
+echo " SSSSSS   Y   SSSSSS    T    EEEEE  M M M M"
+echo "      S   Y        S    T    E      M  M  M"
+echo " SSSSSS   Y   SSSSSS    T    EEEEEE M     M"
+
+make systemimage
