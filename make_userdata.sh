3af2b1ba6c9ba7a315040ace8c1a0917b6b7718b
乐正白芷 <42693943+yuezhengbaizhi@users.noreply.github.com>
2020-02-24 00:25:33 -0500

    Create make_userdata.sh

---
+++ make_userdata.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+. build/envsetup.sh
+lunch hentai_poplar-user
+
+echo " U    U SSSSSS EEEEEE RRRRRR DDDD       A     TTTTTTT     A    "
+echo " U    U S      E      R    R D   D     A A       T       A A   "
+echo " U    U SSSSSS EEEEE  RRRRRR D    D   A   A      T      A   A  "
+echo " U    U      S E      R RR   D   D   AAAAAAA     T     AAAAAAA "
+echo " UUUUUU SSSSSS EEEEEE R   RR DDDD   A       A    T    A       A"
+
+make userdataimage
