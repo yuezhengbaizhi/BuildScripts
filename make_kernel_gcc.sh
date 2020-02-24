bd5c48286723078b031b7e17c0b092c92e377ac0
乐正白芷 <42693943+yuezhengbaizhi@users.noreply.github.com>
2020-02-24 00:31:52 -0500

    Create make_kernel_gcc.sh

---
+++ make_kernel_gcc.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cd kernel/sony/msm-4.14/kernel
+export CROSS_COMPILE_ARM32=~/hentai/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export CROSS_COMPILE=~/hentai/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+
+make ARCH=arm64 CROSS_COMPILE=$CROSS_COMPILE aosp_yoshino_poplar_defconfig O=./out
+
+make ARCH=arm64 CROSS_COMPILE=$CROSS_COMPILE -j4 O=./out
+
+cd ../../../..
+
+echo "In out/arch/arm64/boot, rename image.gz-dtb to kernel-dtb-$(device_name) and place it in kernel/sony/msm-4.14/common-kernel"
