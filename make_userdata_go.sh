53f1d682ea6cf3643e1e1534dabf678f7b12cf34
乐正白芷 <42693943+yuezhengbaizhi@users.noreply.github.com>
2020-02-24 01:21:47 -0500

    Create make_userdata_go.sh

---
+++ make_userdata_go.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cd ~/hentai
+
+. build/envsetup.sh
+lunch hentai_poplar_go-user
+
+echo " U    U SSSSSS EEEEEE RRRRRR DDDD       A     TTTTTTT     A    "
+echo " U    U S      E      R    R D   D     A A       T       A A   "
+echo " U    U SSSSSS EEEEE  RRRRRR D    D   A   A      T      A   A  "
+echo " U    U      S E      R RR   D   D   AAAAAAA     T     AAAAAAA "
+echo " UUUUUU SSSSSS EEEEEE R   RR DDDD   A       A    T    A       A"
+
+make userdataimage
