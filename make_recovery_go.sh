0fd930db50026f904c99ad02119bb381efe708f3
乐正白芷 <42693943+yuezhengbaizhi@users.noreply.github.com>
2020-02-24 01:20:36 -0500

    Create make_recovery_go.sh

---
+++ make_recovery_go.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cd ~/hentai
+
+. build/envsetup.sh
+lunch hentai_poplar_go-user
+
+echo " RRRRRR EEEEEE CCCCCC OOOOOO V       V EEEEEE RRRRRR Y   Y"
+echo " R    R E      C      O    O  V     V  E      R    R  Y Y "
+echo " RRRRRR EEEEE  C      O    O   V   V   EEEEE  RRRRRR   Y  "
+echo " R RR   E      C      O    O    V V    E      R RR     Y  "
+echo " R   RR EEEEEE CCCCCC OOOOOO     V     EEEEEE R   RR   Y  "   
+
+make recoveryimage
