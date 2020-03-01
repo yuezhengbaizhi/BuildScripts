092035d6444d743cc130482650657499187efc57
乐正白芷 <42693943+yuezhengbaizhi@users.noreply.github.com>
2020-02-29 22:22:15 -0500

    Update make.sh

--- make.sh
+++ make.sh
@@ -2,52 +2,52 @@
 
 cd BuildScripts
 
-if [ $1 = --all ]; then
+if [ $1 = "--all" ]; then
     . make_all.sh
 fi
 
-if [ $1 = --all-go ]; then
+if [ $1 = "--all-go" ]; then
     . make_all_go.sh
 fi
-if [ $1 = --boot ]; then
+if [ $1 = "--boot" ]; then
     . make_boot.sh
 fi
-if [ $1 = --boot-go ]; then
+if [ $1 = "--boot-go" ]; then
     . make_boot_go.sh
 fi
-if [ $1 = --cache ]; then
+if [ $1 = "--cache" ]; then
     . make_cache.sh
 fi
-if [ $1 = --cache-go ]; then
+if [ $1 = "--cache-go" ]; then
     . make_cache_go.sh
 fi
-if [ $1 = --recovery ]; then
+if [ $1 = "--recovery" ]; then
     . make_recovery.sh
 fi
-if [ $1 = --recovery-go ]; then
+if [ $1 = "--recovery-go" ]; then
     . make_recovery_go.sh
 fi
-if [ $1 = --system ]; then
+if [ $1 = "--system" ]; then
     . make_system.sh
 fi
-if [ $1 = --system-go ]; then
+if [ $1 = "--system-go" ]; then
     . make_system_go.sh
 fi
-if [ $1 = --userdata ]; then
+if [ $1 = "--userdata" ]; then
     . make_userdata.sh
 fi
-if [ $1 = --userdata-go ]; then
+if [ $1 = "--userdata-go" ]; then
     . make_userdata_go.sh
 fi
-if [ $1 = --vendor ]; then
+if [ $1 = "--vendor" ]; then
     . make_vendor.sh
 fi
-if [ $1 = --vendor-go ]; then
+if [ $1 = "--vendor-go" ]; then
     . make_vendor_go.sh
 fi
-if [ $1 = --kernel-clang ]; then
+if [ $1 = "--kernel-clang" ]; then
     . make_kernel_clang.sh
 fi
-if [ $1 = --kernel-gcc ]; then
+if [ $1 = "--kernel-gcc" ]; then
     . make_kernel_gcc.sh
 fi
